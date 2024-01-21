469080930b9deb80e26a61ec68f0d7f864934550
Brad580 <bradtravers8740@gmail.com>
2024-01-21 15:13:54 -0500

    update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Authenticate from './components/Authenticate';
+import SignUpForm from './components/SignUpForm';
+import './App.css'
+
+
+export default function App() {
+  const [token, setToken] = useState(null);
+
+  return (
+    <>
+      <SignUpForm setToken={setToken} />
+      <Authenticate token={token} />
+    </>
+  );
+}
